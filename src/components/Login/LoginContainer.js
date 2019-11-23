b9cd32943cbdb232da44e4afc5a599fcdc4c496e
girin-dev <kw8799@gmail.com>
2019-11-23 17:24:04 +0900

    [Login] Tooltip 별도로 분리

--- src/components/Login/LoginContainer.js
+++ src/components/Login/LoginContainer.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
-import Login from './Login';
 import introImg from '../../static/img/intro-img.png';
 import introImg2x from '../../static/img/intro-img@2x.png';
 import introImg3x from '../../static/img/intro-img@3x.png';
-import triagle from '../../static/img/triangle.svg';
+import Login from './Login';
+// import triagle from '../../static/img/triangle.svg';
+import Tooltip from  '../Tooltip';
 
 const H1Title = styled.h1`
   display:block;
@@ -17,27 +18,27 @@ const DivPengsu = styled.div`
   margin:0 auto;
   width: 60%
 `;
-const TooltipPengsu = styled.div`
-  position:absolute;
-  top:-40px;
-  right:0;
-  width:88px;
-  box-sizing:border-box;
-  background:#2b3039;
-  color:#fff;
-  font-size:14px;
-  border-radius:5px
-  padding:11px 27px
-  &:before{
-    content:'펭-하';
-  }
-`;
-const TooltipTriangle = styled.img`
-  position:absolute;
-  top: 38px;
-  left: 20px;
-  width: 17px;
-`;
+// const TooltipPengsu = styled.div`
+//   position:absolute;
+//   top:-40px;
+//   right:0;
+//   width:88px;
+//   box-sizing:border-box;
+//   background:#2b3039;
+//   color:#fff;
+//   font-size:14px;
+//   border-radius:5px
+//   padding:11px 27px
+//   &:before{
+//     content:'펭-하';
+//   }
+// `;
+// const TooltipTriangle = styled.img`
+//   position:absolute;
+//   top: 38px;
+//   left: 20px;
+//   width: 17px;
+// `;
 const ImgPengsu = styled.img`
   display:inline-block;
   width:100%;
@@ -56,9 +57,10 @@ return(
           `}
         alt="펭수 이미지">
       </ImgPengsu>
-      <TooltipPengsu>
+      {/* <TooltipPengsu>
         <TooltipTriangle src={triagle} alt="말풍선을 위한 삼각형"/>
-      </TooltipPengsu>
+      </TooltipPengsu> */}
+      <Tooltip property={{msg:'펭-하!'}}/>
      </DivPengsu>
     <Login/>
   </>
---
+++ src/components/Tooltip.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+import triagle from '../static/img/triangle.svg';
+
+const TooltipPengsu = styled.div`
+  position:absolute;
+  top:-40px;
+  right:0;
+  box-sizing:border-box;
+  background:#2b3039;
+  color:#fff;
+  font-size:14px;
+  border-radius:5px;
+  padding:11px 27px;
+`;
+const TooltipTriangle = styled.img`
+  position:absolute;
+  top: 38px;
+  left: 20px;
+  width: 17px;
+`;
+
+function Tooltip({property}) {
+  return(
+    <TooltipPengsu>
+      {property.msg}
+      <TooltipTriangle src={triagle} alt="말풍선을 위한 삼각형"/>
+    </TooltipPengsu>
+  )
+}
+
+export default Tooltip;
