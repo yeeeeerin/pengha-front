e59922bda3f4b09b337bc0cbe497b136f0eb4288
yeeeeerin <ebbunbul@gmail.com>
2019-11-23 12:44:28 +0900

    no message

--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -1,3 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 
+
+const Main = () => {
+    return (
+        <Wrapper>
+
+        </Wrapper>
+    )
+}
+
+export default Main;
+
+const Wrapper = styled.div`
+
+`
