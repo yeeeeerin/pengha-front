2d358f2ae748b349d7b75e59e130f3fdd018356f
girin-dev <kw8799@gmail.com>
2019-11-23 16:30:06 +0900

    [Daily] 일별보기 init

---
+++ src/components/Daily/Daily.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+
+function Daily() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default Daily;
