e15e86d023f27bc004aa38b0ccebba2f54ea4e0a
yeeeeerin <ebbunbul@gmail.com>
2019-11-23 13:28:43 +0900

    중앙 정렬

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from "styled-components";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Wrapper >
+      <Router>
+
+      </Router>
+    </Wrapper>
+  );
+}
+
+export default App;
+
+const Wrapper = styled.div`
+  position:absolute;
+  width:100%;
+  height:100%;
+
+  background:#000;
+
+  
+`
+
---
+++ src/main/Main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+
+const Main = () => {
+
+    return (
+        <Wrapper>
+
+        </Wrapper>
+    )
+}
+
+export default Main;
+
+const Wrapper = styled.div`
+    
+    background:#000;
+`
